c3bae53f2c8f8ecd9027b555fa25e743ca9a0371
DZH <327983969@qq.com>
2021-06-23 23:21:46 +0800

    .

---
+++ src/pages/home/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <view>
+    <home-album></home-album>
+    <home-category></home-category>
+    <home-new></home-new>
+    <home-recommend></home-recommend>
+  </view>
+</template>
+
+<script>
+import HomeAlbum from './homeAlbum/index'
+import HomeNew from './homeNew/index'
+import HomeRecommend from './homeRecommend/index'
+import HomeCategory from './homeCategory/index'
+export default {
+  components: {
+    HomeAlbum,
+    HomeCategory,
+    HomeNew,
+    HomeRecommend
+  }
+}
+</script>
+
+<style>
+</style>
